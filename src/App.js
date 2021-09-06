c39d5491e9cf5a2406cfead749d5a59521fc36d1
Jerry Almeida <jqueryalmeida@gmail.com>
2021-09-06 18:00:35 +0100

    Get emojis via attr

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [who, setWho] = useState('');
+  const [who, setWho] = useState('🐼');
 
-  function handleWho(){
-    setWho('🐯');
+  function handleWho(e){
+    e.preventDefault();
+    const emoji = e.target.getAttribute('data-emoji') || '🐯'
+    setWho(emoji);
 
   }
   return (
     <div className="App">
      <div>💪{who}👊</div>
      <button onClick={handleWho} >Tiger</button>
+     <button onClick={handleWho} data-emoji="🐵">Monkey</button>
+     <button onClick={handleWho} data-emoji="🐷">Pork</button>
     </div>
   );
 }
